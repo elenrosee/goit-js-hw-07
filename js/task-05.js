ea47dd01087f143a757edf11f3f8aff5caeb463c
Elena Ovcharenko <elen_rosee@MacBook-Pro-Ovcarenko.local>
2021-04-19 20:52:58 +0300

    fix task-05

--- js/task-05.js
+++ js/task-05.js
@@ -3,8 +3,13 @@ const refs = {
   greetingName: document.querySelector('#name-output'),
 };
 
+const defaultName = refs.greetingName.textContent;
+
 refs.input.addEventListener('input', onInputChange);
 
 function onInputChange(event) {
   refs.greetingName.textContent = event.currentTarget.value;
+  if (refs.greetingName.textContent.length === 0) {
+    refs.greetingName.textContent = defaultName;
+  }
 }
